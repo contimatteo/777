72367c0f87df51fd5a6b54ea803d0ca2a39fe60b
contimatteo <contimatteo1505@gmail.com>
2017-10-10 09:43:42 +0200

    Add ElementoGrafico class

---
+++ Alpha/777/ElementoGrafico.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Matteo Conti on 10/10/17.
+//
+
+#include "ElementoGrafico.hpp"
+
+
+ElementoGrafico::ElementoGrafico(int stack)
+{
+
+}
---
+++ Alpha/777/ElementoGrafico.hpp
@@ -0,0 +1,28 @@
+//
+// Created by Matteo Conti on 10/10/17.
+//
+
+#ifndef INC_777_ELEMENTOGRAFICO_HPP
+#define INC_777_ELEMENTOGRAFICO_HPP
+
+
+class ElementoGrafico
+{
+    public:
+    /*
+     * 1: erore
+     * 2: pet
+     * 3: nemico
+     * 4: altri
+     * 5: ..
+     * --------------
+     * 10: mura mappa
+     * 11: pavimento mappa
+     * 12: ..
+     */
+    ElementoGrafico(int tipo_personaggio);
+
+};
+
+
+#endif //INC_777_ELEMENTOGRAFICO_HPP
